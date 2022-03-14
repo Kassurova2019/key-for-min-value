deb3791693b80f6297e1f2df2d5234add88f1653
Luba Kassurova <kassurova@icloud.com>
2022-03-14 18:02:40 -0400

    commit tree 9644c1383956d751fbf00cc930f80a54b1342221

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+new_name = nil
+current_min = 1000
+name_hash.each do |name, number|
+    if number < current_min 
+new_name = name
+current_min = number
+    end
+end
+    new_name
 end
